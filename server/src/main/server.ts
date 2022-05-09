cf688cfb2c1d8faccfb165a41d0ba38199c916d6
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-09 01:14:12 -0300

    add port env

--- server/src/main/server.ts
+++ server/src/main/server.ts
@@ -4,6 +4,6 @@ import { setupApp } from '@/main/config'
 
 const app = setupApp()
 
-app.listen(3333, () => {
+app.listen(process.env.PORT ?? 3333, () => {
   console.log('Server running at http://localhost:3333/')
 })
