f8a4b87a5a63f065218a552c618ebcf9e46f382f
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-10 02:57:16 -0300

    things

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,18 @@
+import { AuthProvider } from './context/auth'
 import { Widget } from './components/Widget'
+import { Header } from './components/Header'
+
 
 export function App () {
-  return <Widget />
+  return (
+    <AuthProvider>
+      <Header />
+      <main className='px-40 pt-12'>
+        <div className='bg-zinc-800 py-12 px-14 rounded-md text-zinc-400'>
+          Experimente enviar um feedback de um <i>bug</i> na aplicação 🐛 
+        </div>
+      </main>
+      <Widget />
+    </AuthProvider>
+  )
 }
