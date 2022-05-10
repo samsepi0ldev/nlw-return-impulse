3587d2e5b2b92494d122d863ece09250a548f007
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-10 13:46:38 -0300

    add load feedbacks in repository prisma

--- server/src/infra/db/feedback-prisma-repository.ts
+++ server/src/infra/db/feedback-prisma-repository.ts
@@ -1,9 +1,14 @@
-import { SubmitFeedbackRepository } from '@/data/protocols/db'
-import { SubmitFeedback } from '@/domain/use-cases'
+import { SubmitFeedbackRepository, LoadFeedbacksRepository } from '@/data/protocols/db'
+import { LoadFeedbacks, SubmitFeedback } from '@/domain/use-cases'
 import { prisma } from '@/main/prisma'
 
-export class FeedbackPrismaRepository implements SubmitFeedbackRepository {
+export class FeedbackPrismaRepository implements SubmitFeedbackRepository, LoadFeedbacksRepository {
   async create (input: SubmitFeedback.Input): Promise<void> {
     await prisma.feedback.create({ data: input })
+  }
+
+  async load (): Promise<LoadFeedbacks.Output> {
+    const feedbacks = await prisma.feedback.findMany()
+    return feedbacks
   }
 }
