7752037ed8ce49ee8bbce236000132c98490b8a4
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-10 19:54:13 -0300

    created Context component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,24 @@
-import { AuthProvider } from './context/auth'
+import { AuthProvider, useAuth } from './context/auth'
 import { Widget } from './components/Widget'
 import { Header } from './components/Header'
+import { FeedbackType } from './components/WidgetForm'
+import { useEffect, useState } from 'react'
+import { Content } from './components/Content'
 
+export interface FeedbackData {
+  id: string
+  type: FeedbackType
+  comment: string
+  screenshot: string | null
+}
 
 export function App () {
+  const [feedbacks, setFeedbacks] = useState<FeedbackData[]>([])
+
   return (
     <AuthProvider>
-      <Header />
-      <main className='w-full px-6 pt-6 text-center md:text-left md:pt-12 md:px-40'>
-        <div className='bg-zinc-800 py-12 px-14 rounded-md text-zinc-400'>
-          Experimente enviar um feedback de um <i>bug</i> na aplicação 🐛 
-        </div>
-      </main>
+      <Header feedbacksParamSetter={setFeedbacks} />
+      <Content feedbacks={feedbacks} />
       <Widget />
     </AuthProvider>
   )
