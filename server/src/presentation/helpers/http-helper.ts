773b70898ffba70e6b68373c62ca253ea0ad36aa
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-10 13:49:01 -0300

    add feature "ok" in http helper

--- server/src/presentation/helpers/http-helper.ts
+++ server/src/presentation/helpers/http-helper.ts
@@ -1,16 +1,27 @@
 import { HttpResponse, StatusCode } from '@/presentation/protocols/http'
+import { ServerError } from '@/presentation/errors'
+
+export const ok = <T = any> (data: T): HttpResponse<T> => ({
+  statusCode: StatusCode.ok,
+  body: data
+})
+
+export const created = (): HttpResponse => ({
+  statusCode: StatusCode.created,
+  body: null
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: StatusCode.noContent,
+  body: null
+})
 
 export const badRequest = (error: Error): HttpResponse<Error> => ({
   statusCode: StatusCode.badRequest,
   body: error
 })
 
-export const serverError = (error: Error): HttpResponse<Error> => ({
+export const serverError = (error: unknown): HttpResponse<Error> => ({
   statusCode: StatusCode.serverError,
-  body: error
-})
-
-export const created = (): HttpResponse => ({
-  statusCode: StatusCode.created,
-  body: null
+  body: new ServerError(error instanceof Error ? error : undefined)
 })
