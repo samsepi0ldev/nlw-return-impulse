9c764f3371441511b78d9e3f8e70b21715728a44
Elivelton Santos <samsepi0l.dev@gmail.com>
2022-05-10 13:49:58 -0300

    add route to get all feedbacks

--- server/src/main/routes/feedbacks-routes.ts
+++ server/src/main/routes/feedbacks-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 
 import { adaptExpressRoute } from '@/main/adapters'
-import { makeSubmitFeedbackController } from '@/main/factories/controllers'
+import { makeSubmitFeedbackController, makeLoadFeedbacksController } from '@/main/factories/controllers'
 
 export default (router: Router): void => {
   router.post('/feedbacks', adaptExpressRoute(makeSubmitFeedbackController()))
+  router.get('/feedbacks', adaptExpressRoute(makeLoadFeedbacksController()))
 }
